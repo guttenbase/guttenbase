e4e2c66ca26488adf5a4e298495f9f5f0da39ae3
Markus Dahm <markus.dahm@akquinet.de>
2024-05-13 12:44:31 +0200

    WrapperConnectorInfo

---
+++ src/main/kotlin/io/github/guttenbase/connector/impl/WrapperConnectorInfo.kt
@@ -0,0 +1,45 @@
+package io.github.guttenbase.connector.impl
+
+import io.github.guttenbase.connector.Connector
+import io.github.guttenbase.connector.ConnectorInfo
+import io.github.guttenbase.connector.DatabaseType
+import io.github.guttenbase.repository.ConnectorRepository
+import java.sql.Connection
+
+typealias CLOSER = (Connection) -> Unit
+
+/**
+ * Connection info via alternate connection provider
+ *
+ *  &copy; 2012-2034 akquinet tech@spree
+ *
+ *
+ * @author M. Dahm
+ */
+@Suppress("unused")
+open class WrapperConnectorInfo @JvmOverloads constructor(
+  override val schema: String,
+  override val databaseType: DatabaseType,
+  private val connectionProvider: () -> Connection,
+  private val connectionCloser: CLOSER? = null
+) : ConnectorInfo {
+
+  override val user: String = ""
+  override val password: String = ""
+
+  override fun createConnector(connectorRepository: ConnectorRepository, connectorId: String): Connector =
+    object : AbstractConnector(connectorRepository, connectorId, this@WrapperConnectorInfo) {
+      override fun openConnection(): Connection {
+        connection = connectionProvider.invoke()
+        return connection
+      }
+
+      override fun closeConnection() {
+        if (connectionCloser != null) {
+          connectionCloser.invoke(connection)
+        } else {
+          super.closeConnection()
+        }
+      }
+    }
+}
