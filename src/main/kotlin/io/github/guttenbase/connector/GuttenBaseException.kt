2a8204581996e5d68ddd77bf425bbbe2a40e0bd8
Markus Dahm <markus.dahm@akquinet.de>
2025-01-07 10:19:05 +0100

    ProprietaryColumnTypeDefinitionFactory

--- src/main/kotlin/io/github/guttenbase/connector/GuttenBaseException.kt
+++ src/main/kotlin/io/github/guttenbase/connector/GuttenBaseException.kt
@@ -4,7 +4,4 @@ open class GuttenBaseException : RuntimeException {
   constructor(message: String) : super(message)
   constructor(message: String, cause: Throwable) : super(message, cause)
 
-  companion object {
-    private const val serialVersionUID = 1L
-  }
 }
