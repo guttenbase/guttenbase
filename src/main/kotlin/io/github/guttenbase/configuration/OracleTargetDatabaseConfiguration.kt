147bfb4dccaa9461f574f7b92da9e561047d8e97
Markus Dahm <markus.dahm@akquinet.de>
2025-01-08 13:52:26 +0100

    Oracle fixed

--- src/main/kotlin/io/github/guttenbase/configuration/OracleTargetDatabaseConfiguration.kt
+++ src/main/kotlin/io/github/guttenbase/configuration/OracleTargetDatabaseConfiguration.kt
@@ -56,40 +56,25 @@ open class OracleTargetDatabaseConfiguration(connectorRepository: ConnectorRepos
       val foreignKeyNames: RESULT_LIST = ScriptExecutorTool(connectorRepository).executeQuery(
         connectorId,
         """SELECT DISTINCT AC.OWNER, AC.TABLE_NAME, AC.CONSTRAINT_NAME FROM ALL_CONSTRAINTS AC, ALL_CONS_COLUMNS ACC 
-          |WHERE AC.CONSTRAINT_TYPE = 'R' AND ACC.TABLE_NAME IN ($tablesList) AND ACC.OWNER = '$schema' 
+          |WHERE AC.CONSTRAINT_TYPE = 'R' AND ACC.TABLE_NAME IN $tablesList AND ACC.OWNER = '$schema' 
           |AND ACC.CONSTRAINT_NAME = AC.R_CONSTRAINT_NAME AND ACC.OWNER = AC.R_OWNER""".trimMargin()
       )
 
-      // memorize any problems that occur during constraint handling
-      val problems = StringBuilder()
       for (fkMap in foreignKeyNames) {
         val tableName = fkMap["TABLE_NAME"].toString()
         val constraintName = fkMap["CONSTRAINT_NAME"].toString()
         val owner = fkMap["OWNER"].toString()
-        val sql =
-          "ALTER TABLE " + owner + "." + tableName + (if (enable) " ENABLE " else " DISABLE ") + "CONSTRAINT " + constraintName
+        val sql = """ALTER TABLE $owner."$tableName"""" +
+            (if (enable) " ENABLE " else " DISABLE ") +
+            """CONSTRAINT "$constraintName""""
 
-        try {
-          executeSQL(connection, sql)
-        } catch (e: SQLException) {
-          LOG.error("Unable to handle constraint: $sql", e)
-          problems.append("Unable to handle constraint: ").append(sql).append("->").append(e.message).append(":")
-            .append(e.sqlState)
-        }
-      }
-
-      if (problems.isNotEmpty()) {
-        // if there has been a problem with any constraint, now throw the exception
-        throw SQLException("Constraint problems occurred: $problems")
+        executeSQL(connection, sql)
       }
     }
   }
 
   companion object {
-    @JvmStatic
-    private val LOG = LoggerFactory.getLogger(OracleTargetDatabaseConfiguration::class.java)
-
     private fun createTablesList(tableMetaDatas: List<TableMetaData>) =
-      tableMetaDatas.joinToString(separator = ", ", prefix = "'", postfix = "'")
+      tableMetaDatas.joinToString(separator = ", ", prefix = "(", postfix = ")") { "'${it.tableName}'" }
   }
 }
