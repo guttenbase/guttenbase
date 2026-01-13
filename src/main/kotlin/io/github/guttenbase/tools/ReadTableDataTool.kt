34bc380e2358734e79d69719a93ebbb2a2ce4ca7
Markus Dahm <markus.dahm@spree.de>
2026-01-13 09:09:27 +0100

    jvm

--- src/main/kotlin/io/github/guttenbase/tools/ReadTableDataTool.kt
+++ src/main/kotlin/io/github/guttenbase/tools/ReadTableDataTool.kt
@@ -21,99 +21,100 @@ import java.util.*
  * @author M. Dahm
  */
 open class ReadTableDataTool(
-  private val connectorRepository: ConnectorRepository,
-  private val connectorId: String,
-  private val tableMetaData: DatabaseEntityMetaData,
+	private val connectorRepository: ConnectorRepository,
+	private val connectorId: String,
+	private val tableMetaData: DatabaseEntityMetaData,
 ) : AutoCloseable {
-  constructor(connectorRepository: ConnectorRepository, connectorId: String, tableName: String, view: Boolean = false) : this(
-    connectorRepository, connectorId,
-    if (view)
-      connectorRepository.getDatabase(connectorId).getView(tableName) ?: throw IllegalStateException("View $tableName not found")
-    else
-      connectorRepository.getDatabase(connectorId).getTable(tableName) ?: throw IllegalStateException("Table $tableName not found")
-  )
+	@JvmOverloads
+	constructor(connectorRepository: ConnectorRepository, connectorId: String, tableName: String, view: Boolean = false) : this(
+		connectorRepository, connectorId,
+		if (view)
+			connectorRepository.getDatabase(connectorId).getView(tableName) ?: throw IllegalStateException("View $tableName not found")
+		else
+			connectorRepository.getDatabase(connectorId).getTable(tableName) ?: throw IllegalStateException("Table $tableName not found")
+	)
 
-  private lateinit var connector: Connector
-  private lateinit var resultSet: ResultSet
+	private lateinit var connector: Connector
+	private lateinit var resultSet: ResultSet
 
-  fun start(): ReadTableDataTool {
-    if (!this::connector.isInitialized) {
-      connector = connectorRepository.createConnector(connectorId)
+	fun start(): ReadTableDataTool {
+		if (!this::connector.isInitialized) {
+			connector = connectorRepository.createConnector(connectorId)
 
-      start(connector.openConnection())
-    }
+			start(connector.openConnection())
+		}
 
-    return this
-  }
+		return this
+	}
 
-  fun start(connection: Connection): ReadTableDataTool {
-    val sourceConfiguration = connectorRepository.getSourceDatabaseConfiguration(connectorId)
-    val tableMapper = connectorRepository.hint<TableMapper>(connectorId)
-    val databaseMetaData = connectorRepository.getDatabase(connectorId)
-    val tableName = tableMapper.fullyQualifiedTableName(tableMetaData, databaseMetaData)
-    val selectStatement = SelectStatementCreator(connectorRepository, connectorId)
-      .createSelectStatement(connection, tableName, tableMetaData)
+	fun start(connection: Connection): ReadTableDataTool {
+		val sourceConfiguration = connectorRepository.getSourceDatabaseConfiguration(connectorId)
+		val tableMapper = connectorRepository.hint<TableMapper>(connectorId)
+		val databaseMetaData = connectorRepository.getDatabase(connectorId)
+		val tableName = tableMapper.fullyQualifiedTableName(tableMetaData, databaseMetaData)
+		val selectStatement = SelectStatementCreator(connectorRepository, connectorId)
+			.createSelectStatement(connection, tableName, tableMetaData)
 
-    selectStatement.fetchSize = 512
-    sourceConfiguration.beforeSelect(connection, connectorId, tableMetaData)
-    resultSet = selectStatement.executeQuery()
-    sourceConfiguration.afterSelect(connection, connectorId, tableMetaData)
+		selectStatement.fetchSize = 512
+		sourceConfiguration.beforeSelect(connection, connectorId, tableMetaData)
+		resultSet = selectStatement.executeQuery()
+		sourceConfiguration.afterSelect(connection, connectorId, tableMetaData)
 
-    return this
-  }
+		return this
+	}
 
-  fun end() {
-    if (this::connector.isInitialized) {
-      end(connector.openConnection())
-      connector.closeConnection()
-    }
-  }
+	fun end() {
+		if (this::connector.isInitialized) {
+			end(connector.openConnection())
+			connector.closeConnection()
+		}
+	}
 
-  fun end(connection: Connection) {
-    val sourceConfiguration = connectorRepository.getSourceDatabaseConfiguration(connectorId)
-    sourceConfiguration.finalizeSourceConnection(connection, connectorId)
-    resultSet.close()
-  }
+	fun end(connection: Connection) {
+		val sourceConfiguration = connectorRepository.getSourceDatabaseConfiguration(connectorId)
+		sourceConfiguration.finalizeSourceConnection(connection, connectorId)
+		resultSet.close()
+	}
 
-  /**
-   * @param noLines -1 means read all lines
-   * @return list of maps containing table data or null of no more data is available
-   */
-  fun readTableData(noLines: Int): List<Map<String, Any?>> {
-    var lines = noLines
-    val result = ArrayList<Map<String, Any?>>()
-    val sourceColumnNameMapper = connectorRepository.hint<ColumnMapper>(connectorId)
-    val orderedSourceColumns = ColumnOrderHint.getSortedColumns(connectorRepository, tableMetaData)
+	/**
+	 * @param noLines -1 means read all lines
+	 * @return list of maps containing table data or null of no more data is available
+	 */
+	fun readTableData(noLines: Int): List<Map<String, Any?>> {
+		var lines = noLines
+		val result = ArrayList<Map<String, Any?>>()
+		val sourceColumnNameMapper = connectorRepository.hint<ColumnMapper>(connectorId)
+		val orderedSourceColumns = ColumnOrderHint.getSortedColumns(connectorRepository, tableMetaData)
 
-    if (lines < 0) {
-      lines = Int.MAX_VALUE
-    }
+		if (lines < 0) {
+			lines = Int.MAX_VALUE
+		}
 
-    var rowIndex = 0
+		var rowIndex = 0
 
-    while (rowIndex < lines && resultSet.next()) {
-      val rowData = TreeMap<String, Any?>()
+		while (rowIndex < lines && resultSet.next()) {
+			val rowData = TreeMap<String, Any?>()
 
-      for (columnIndex in 1..orderedSourceColumns.size) {
-        val sourceColumn = orderedSourceColumns[columnIndex - 1]
-        val columnName = sourceColumnNameMapper.mapColumnName(sourceColumn, tableMetaData)
-        val columnMapping = ColumnDataMappingTool(connectorRepository).getCommonColumnTypeMapping(
-          sourceColumn, sourceColumn
-        ) ?: throw IllegalStateException("Type mapping not found for $sourceColumn")
-        val data = columnMapping.sourceColumnType.getValue(resultSet, columnIndex, sourceColumn)
-        val mappedData = columnMapping.columnDataMapper.map(columnMapping, data)
+			for (columnIndex in 1..orderedSourceColumns.size) {
+				val sourceColumn = orderedSourceColumns[columnIndex - 1]
+				val columnName = sourceColumnNameMapper.mapColumnName(sourceColumn, tableMetaData)
+				val columnMapping = ColumnDataMappingTool(connectorRepository).getCommonColumnTypeMapping(
+					sourceColumn, sourceColumn
+				) ?: throw IllegalStateException("Type mapping not found for $sourceColumn")
+				val data = columnMapping.sourceColumnType.getValue(resultSet, columnIndex, sourceColumn)
+				val mappedData = columnMapping.columnDataMapper.map(columnMapping, data)
 
-        rowData[columnName] = mappedData
-      }
+				rowData[columnName] = mappedData
+			}
 
-      result.add(rowData)
-      rowIndex++
-    }
+			result.add(rowData)
+			rowIndex++
+		}
 
-    return result
-  }
+		return result
+	}
 
-  override fun close() {
-    end()
-  }
+	override fun close() {
+		end()
+	}
 }
